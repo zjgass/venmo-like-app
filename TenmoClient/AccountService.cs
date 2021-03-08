ef4727644304abfecb9bb6bcb1676ac2e2202341
Zachary <zjgass@gmail.com>
2021-03-07 22:31:12 -0500

    Fixed error handling an created SuperService.

--- TenmoClient/AccountService.cs
+++ TenmoClient/AccountService.cs
@@ -7,11 +7,8 @@ using TenmoClient.Data;
 
 namespace TenmoClient
 {
-    public class AccountService
+    public class AccountService : SuperService
     {
-        private readonly static string API_BASE_URL = "https://localhost:44315/";
-        private readonly IRestClient client = new RestClient();
-
         public API_Account GetAccountBalance()
         {
             client.Authenticator = new JwtAuthenticator(UserService.GetToken());
